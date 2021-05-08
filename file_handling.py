8f95d045cbb571af200ed47aed7105c65b3f423a
SykesZachary <zachary.h.sykes@gmail.com>
2021-05-08 20:39:38 +0100

    Sparated file processing onto its own file

--- file_handling.py
+++ file_handling.py
@@ -4,30 +4,7 @@
 from Bio import SeqIO
 import os
 
-from seq_manipulation import SeqManipulation
 
-# PATH set
-proj_path = os.path.abspath(os.getcwd())
-sars_cov_2_aa = os.path.join(proj_path, 'SARS_CoV_2_aaSeq/')
-sars_cov_2_dna = os.path.join(proj_path, 'SARS_CoV_2_dnaSeq/')
-
-# Human codon table for amino acids
-hsap_codon_table = {'F': ['UUU', 'UUC'],
-                    'L': ['UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'],
-                    'S': ['UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'],
-                    'Y': ['UAU', 'UAC'], 'Stop': ['UAA', 'UAG', 'UGA'],
-                    'C': ['UGU', 'UGC'], 'W': ['UGG'],
-                    'P': ['CCU', 'CCC', 'CCA', 'CCG'],
-                    'H': ['CAU', 'CAC'], 'Q': ['CAA', 'CAG'],
-                    'R': ['CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],
-                    'I': ['AUU', 'AUC', 'AUA'], 'M': ['AUG'],
-                    'T': ['ACU', 'ACC', 'ACA', 'ACG'], 'N': ['AAU', 'AAC'],
-                    'K': ['AAA', 'AAG'], 'V': ['GUU', 'GUC', 'GUA', 'GUG'],
-                    'A': ['GCU', 'GCC', 'GCA', 'GCG'], 'D': ['GAU', 'GAC'],
-                    'E': ['GAA', 'GAG'], 'G': ['GGU', 'GGC', 'GGA', 'GGG']}
-
-
-# Pulls sequences from files, (.fasta, or .txt)
 def data_extraction(path, file):
     # CWD to sequence file
     os.chdir(path)
@@ -52,16 +29,18 @@ def data_extraction(path, file):
         return data
 
 
-def file_output(path, filename):
-    pass
+def file_output(path, filename, data):
+    # CWD to output directory
+    os.chdir(path)
 
+    # Opening output file and writing to it
+    out_file = open(filename, 'w+')
+    out_file.write(filename)
+    out_file.write('\n')
 
-# Storing sequence data
-seq = data_extraction(sars_cov_2_dna, 'sars_cov_2_s2p_dna.fasta')
+    # Splitting the lines into lengths of 80 aa or bases
+    for idx, item in enumerate(data):
+        out_file.write(item)
+        if (idx + 1) / 80 in range(1, 101):
+            out_file.write('\n')
 
-# Class initialization
-sm = SeqManipulation(seq[0])
-
-print(sm.translate(codons=hsap_codon_table, seq_type='dna'))
-print()
-print(sm.transcribe())
